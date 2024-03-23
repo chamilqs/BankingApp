dc4dab4be421324410b600873aff339309570a48
Samil <114354322+chamilqs@users.noreply.github.com>
2024-03-23 14:26:16 -0400

    [NEW]: Added transfers controller with its dependencies.

---
+++ BankingApp/Controllers/TransfersController.cs
@@ -0,0 +1,26 @@
+﻿using BankingApp.Core.Application.DTOs.Account;
+using BankingApp.Core.Application.Interfaces.Services;
+using BankingApp.Core.Application.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankingApp.Controllers
+{
+    public class TransfersController : Controller
+    {
+        private readonly ITransfersService _transfersService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly AuthenticationResponse user;
+        public TransfersController(ITransfersService transfersService, IHttpContextAccessor httpContextAccessor) 
+        { 
+            _transfersService = transfersService;
+            _httpContextAccessor = httpContextAccessor;
+            user = _httpContextAccessor.HttpContext.Session.Get<AuthenticationResponse>("user");
+        
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
