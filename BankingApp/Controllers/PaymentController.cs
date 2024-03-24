1d97544c1dbebe9d5b4ef042efdcc38cb9267aa1
OAGgithub <oniel.gg@gmail.com>
2024-03-23 20:20:23 -0400

    [NEW] : Add Payment Service

---
+++ BankingApp/Controllers/PaymentController.cs
@@ -0,0 +1,24 @@
+﻿using BankingApp.Core.Application.DTOs.Account;
+using BankingApp.Core.Application.Helpers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankingApp.Controllers
+{
+    public class PaymentController : Controller
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly AuthenticationResponse user;
+
+        public PaymentController(IHttpContextAccessor httpContextAccessor, AuthenticationResponse user)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            user = _httpContextAccessor.HttpContext.Session.Get<AuthenticationResponse>("user");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
