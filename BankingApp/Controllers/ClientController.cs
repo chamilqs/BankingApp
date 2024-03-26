1a1d7bec15dcaf4c4389f843ed34e54272738226
OAGgithub <oniel.gg@gmail.com>
2024-03-25 21:48:05 -0400

    Client Dashboard

--- BankingApp/Controllers/ClientController.cs
+++ BankingApp/Controllers/ClientController.cs
@@ -4,6 +4,8 @@ using BankingApp.Core.Application.Helpers;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using BankingApp.Core.Application.Dtos.Account;
+using BankingApp.Core.Application.Interfaces.Services;
 
 namespace BankingApp.Controllers
 {
@@ -11,14 +13,18 @@ namespace BankingApp.Controllers
     public class ClientController : Controller
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IUserService _userService;
+        private readonly IClientService _clientService;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly AuthenticationResponse authViewModel;
 
-        public ClientController(UserManager<ApplicationUser> userManager, IHttpContextAccessor httpContextAccessor)
+        public ClientController(UserManager<ApplicationUser> userManager, IHttpContextAccessor httpContextAccessor, IClientService clientService, IUserService userService)
         {
             _userManager = userManager;
             _httpContextAccessor = httpContextAccessor;
             authViewModel = _httpContextAccessor.HttpContext.Session.Get<AuthenticationResponse>("user");
+            _clientService = clientService;
+            _userService = userService;
 
         }
 
@@ -27,9 +33,27 @@ namespace BankingApp.Controllers
             return View();
         }
 
-        public IActionResult Dashboard()
+        public async Task<IActionResult> Dashboard(string? userId = null, bool hasError = false, string? message = null)
         {
-            return View();
+            try
+            {
+                GenericResponse response = new()
+                {
+                    HasError = hasError,
+                };
+
+                if (hasError)
+                    response.Error = message;
+                ViewBag.Response = response;
+
+                return View(await _clientService.GetAllProducts(userId));
+            }
+            catch (Exception ex)
+            {
+                var user = await _userService.GetById(userId);
+
+                return RedirectToRoute(new { controller = "Client", action = "Dashboard", userId = user.Id, hasError = true, message = $"An error has occured trying to get the products of the user: {user.Username}" });
+            }
         }
 
     }
